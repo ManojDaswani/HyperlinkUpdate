5c225465781064eebd4f80c94924804c420505c0
Manoj Daswani <ManojDaswani11@gmail.com>
2020-11-05 09:25:17 +0530

    Hyperlink update 5th Nov 2020

--- Hyperlink/ViewController.swift
+++ Hyperlink/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateTextView()
+        
     }
 
-
+    func updateTextView() {
+        let path = "https://wordpress.com/tos"
+        let text = textView?.text ?? ""
+        let attributedString = NSAttributedString.makeHyperlink(for: path, in: text, as: "Terms of Service")
+        let font = textView.font
+        textView.font = .preferredFont(forTextStyle: .body)
+        let textColor = textView.textColor
+        textView?.attributedText = attributedString
+        textView.font = font
+        textView.textColor = textColor
+        
+    }
 }
+
+
 
